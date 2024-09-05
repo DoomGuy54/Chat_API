b3214c48d7b5d00530cbba8fcdebff378adb1ad5
Ryan <ryan10.gugu@gmail.com>
2024-09-04 23:24:45 -0300

    Atualizacao 04/09/24

--- src/models/db.js
+++ src/models/db.js
@@ -5,7 +5,7 @@ let singleton;
 async function connect() {
     if(singleton) return singleton;
 
-    const client = new MongoClient(process.env.DB_host);
+    const client = new MongoClient(process.env.DB_HOST);
     await client.connect();
 
     singleton = client.db(process.env.DB);
--- src/models/salaModel.js
+++ src/models/salaModel.js
@@ -1,8 +1,22 @@
 const db = require("./db");
 
-let listarSalas = async () =>{
+function listarSalas(){
+    return [
+        {
+            "_id":{
+                "$oid": "123456"
+            },
+            "nome": "Guerreiros do Java",
+            "tipo": "Pública"
+        }
+    ]
+}
+
+
+
+/*let listarSalas = async () =>{
     let salas = await db.findAll("salas");
     return salas;
-};
+};*/
 
 module.exports = {listarSalas};
